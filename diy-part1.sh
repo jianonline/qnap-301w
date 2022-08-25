400196a22da35c258489842b7f81b81d010bbcfe
Hitomi Saya <muyinshi@gmail.com>
2022-08-25 14:26:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,6 +18,9 @@ sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generat
 # 修改主机名字
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='QNAP-301W'' package/lean/default-settings/files/zzz-default-settings
 
+# 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
